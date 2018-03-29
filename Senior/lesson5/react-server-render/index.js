5151538024245ea3e87cde6ff608c3ed249b051b
worldsong <2060884089@qq.com>
2018-03-29 17:14:08 +0800

    解决问题：点击文字的话，不会切换颜色 - 因为返回的只是一个 div 块，没有引用任何脚本。

--- Senior/lesson5/react-server-render/index.js
+++ Senior/lesson5/react-server-render/index.js
@@ -4,10 +4,30 @@ const path = require('path')
 const ReactDOMServer = require('react-dom/server')
 const React = require('react')
 const App = require('./src/App.js')
+function genHTML (body) {
+    return `
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta name="author" content="worldsong" />
+    <meta name="website" content="http://songfens.club/" />
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>React 服务器端渲染</title>
+  </head>
+  <body>
+    <div id="root">${body}</div>
+    <script src="dist/main.js"></script>
+  </body>
+  </html>
+  `
+}
 const server = http.createServer(function (request, response) {
     if (request.url === '/') {
+        let body = ReactDOMServer.renderToString(React.createElement(App))
         response.writeHead(200, { 'Content-Type': 'text/html' })
-        response.write(ReactDOMServer.renderToString(React.createElement(App)))
+        response.write(genHTML(body))
         response.end()
     } else {
         if (/\.js/.test(request.url)) {
