8cfeb792f340204c835690631da2f07879754a9b
worldsong <2060884089@qq.com>
2018-03-29 17:06:25 +0800

    区分不同的请求，分别响应html，js。

--- Senior/lesson5/react-server-render/index.js
+++ Senior/lesson5/react-server-render/index.js
@@ -2,8 +2,19 @@ const http = require('http')
 const fs = require('fs')
 const path = require('path')
 const server = http.createServer(function (request, response) {
-    response.writeHead(200, { 'Content-Type': 'text/html' })
-    fs.createReadStream(path.join(__dirname, 'index.html')).pipe(response)
+    if (request.url === '/') {
+        response.writeHead(200, { 'Content-Type': 'text/html' })
+        fs.createReadStream(path.join(__dirname, 'index.html')).pipe(response)
+    } else {
+        if (/\.js/.test(request.url)) {
+            // js file
+            response.writeHead(200, { 'Content-Type': 'application/javascript' })
+            fs.createReadStream(path.join(__dirname, `${request.url}`)).pipe(response)
+        } else {
+            response.writeHead(404)
+            response.end()
+        }
+    }
 })
 server.listen(4200)
 console.log('Server is listening on 4200')
