bc2dca8bb7006954a1fb00f3fde76a9740eff0eb
worldsong <2060884089@qq.com>
2018-03-29 17:03:28 +0800

    搭建node服务器程序，返回html页面。

--- Senior/lesson5/react-server-render/index.js
+++ Senior/lesson5/react-server-render/index.js
@@ -1,8 +1,9 @@
 const http = require('http')
+const fs = require('fs')
+const path = require('path')
 const server = http.createServer(function (request, response) {
     response.writeHead(200, { 'Content-Type': 'text/html' })
-    response.write('hello react.js')
-    response.end()
+    fs.createReadStream(path.join(__dirname, 'index.html')).pipe(response)
 })
 server.listen(4200)
 console.log('Server is listening on 4200')
