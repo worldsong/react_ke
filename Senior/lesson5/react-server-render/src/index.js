088b99b5c97c3f6cd7e7d409c55c8061f4662e02
worldsong <2060884089@qq.com>
2018-03-29 17:17:19 +0800

    解除警告：React 16 里引入 ReactDOM.hydrate() 方法，用于在服务器端渲染的场景中替代 ReactDOM.render()。 重新打包代码： npx webpack --mode development

--- Senior/lesson5/react-server-render/src/index.js
+++ Senior/lesson5/react-server-render/src/index.js
@@ -1,4 +1,4 @@
 const React = require('react')
 const ReactDOM = require('react-dom')
 const App = require('./App')
-ReactDOM.render(React.createElement(App), document.getElementById('root'))
+ReactDOM.hydrate(React.createElement(App), document.getElementById('root'))
