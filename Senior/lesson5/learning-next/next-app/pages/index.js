91d88c823ec537bd2ef043d9977207f8cdb38417
worldsong <2060884089@qq.com>
2018-03-30 15:44:44 +0800

    替换 (replace)而非追加(push)路由 url。

--- Senior/lesson5/learning-next/next-app/pages/index.js
+++ Senior/lesson5/learning-next/next-app/pages/index.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default () =>
     <div>
         Click{' '}
-        <Link href={{ pathname: '/about', query: { name: 'Zeit' } }}>
+        <Link href="/about" replace>
             <a>here</a>
         </Link>{' '}
         to read more
