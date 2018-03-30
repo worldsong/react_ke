64276680e4fff09b25324d7f8cab5f4f707b0341
worldsong <2060884089@qq.com>
2018-03-30 14:40:48 +0800

    为无状态(stateless)组件自定义 getInitialProps生命周期方法，服务端fetch需要引入node-fetch。

--- Senior/lesson5/learning-next/next-app/pages/index.js
+++ Senior/lesson5/learning-next/next-app/pages/index.js
@@ -1,16 +1,13 @@
-import React from 'react'
+const fetch = require('node-fetch');
+const Page = ({ stars }) =>
+    <div>
+        Next stars: {stars}
+    </div>
 
-export default class extends React.Component {
-    static async getInitialProps({ req }) {
-        const userAgent = req ? req.headers['user-agent'] : navigator.userAgent
-        return { userAgent }
-    }
-
-    render() {
-        return (
-            <div>
-                Hello World {this.props.userAgent}
-            </div>
-        )
-    }
+Page.getInitialProps = async ({ req }) => {
+    const res = await fetch('https://api.github.com/repos/zeit/next.js')
+    const json = await res.json()
+    return { stars: json.stargazers_count }
 }
+
+export default Page
