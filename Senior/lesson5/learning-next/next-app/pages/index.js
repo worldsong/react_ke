5f3c60225dd0701dd345317778fe7f90bfe03ee6
worldsong <2060884089@qq.com>
2018-03-30 15:55:13 +0800

    命令式路由：路由事件。 routeChangeStart(url) - 当路由刚开始切换的时候触发 routeChangeComplete(url) - 当路由切换完成时触发 routeChangeError(err, url) - 当路由切换发生错误时触发 beforeHistoryChange(url) - 在改变浏览器 history之前触发 appUpdated(nextRoute) - 当切换页面的时候，应用版本刚好更新的时触发(例如在部署期间切换路由)

--- Senior/lesson5/learning-next/next-app/pages/index.js
+++ Senior/lesson5/learning-next/next-app/pages/index.js
@@ -5,7 +5,9 @@ const handler = () =>
         pathname: '/about',
         query: { name: 'Zeit' }
     })
-
+Router.onRouteChangeStart = url => {
+    console.log('App is changing to: ', url)
+}
 export default () =>
     <div>
         Click <span onClick={handler}>here</span> to read more
