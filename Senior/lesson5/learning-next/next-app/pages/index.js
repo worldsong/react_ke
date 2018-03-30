560e1451b20afc42082b043bb3a7dfc89b1774d7
worldsong <2060884089@qq.com>
2018-03-30 15:48:39 +0800

    让组件支持 onClick事件。

--- Senior/lesson5/learning-next/next-app/pages/index.js
+++ Senior/lesson5/learning-next/next-app/pages/index.js
@@ -3,8 +3,7 @@ import Link from 'next/link'
 export default () =>
     <div>
         Click{' '}
-        <Link href="/about" replace>
-            <a>here</a>
-        </Link>{' '}
-        to read more
+        <Link href="/about">
+            <img src="/static/react_合并更新.png" />
+        </Link>
     </div>
