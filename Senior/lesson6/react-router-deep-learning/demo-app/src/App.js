64477c80621eadd440cef31998f9a2c0746e381f
worldsong <2060884089@qq.com>
2018-04-04 17:05:20 +0800

    router-deep-learning:过渡动画。

--- Senior/lesson6/react-router-deep-learning/demo-app/src/App.js
+++ Senior/lesson6/react-router-deep-learning/demo-app/src/App.js
@@ -1,75 +1,116 @@
 import React from 'react'
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
 import {
     BrowserRouter as Router,
     Route,
-    Link
+    Link,
+    Redirect
 } from 'react-router-dom'
 
-// Each logical "route" has two components, one for
-// the sidebar and one for the main area. We want to
-// render both of them in different places when the
-// path matches the current URL.
-const routes = [
-    { path: '/',
-        exact: true,
-        sidebar: () => <div>home!</div>,
-        main: () => <h2>Home</h2>
-    },
-    { path: '/bubblegum',
-        sidebar: () => <div>bubblegum!</div>,
-        main: () => <h2>Bubblegum</h2>
-    },
-    { path: '/shoelaces',
-        sidebar: () => <div>shoelaces!</div>,
-        main: () => <h2>Shoelaces</h2>
-    }
-]
+/* you'll need this CSS somewhere
+.fade-enter {
+  opacity: 0;
+  z-index: 1;
+}
 
-const SidebarExample = () => (
+.fade-enter.fade-enter-active {
+  opacity: 1;
+  transition: opacity 250ms ease-in;
+}
+*/
+
+const AnimationExample = () => (
     <Router>
-        <div style={{ display: 'flex' }}>
-            <div style={{
-                padding: '10px',
-                width: '40%',
-                background: '#f0f0f0'
-            }}>
-                <ul style={{ listStyleType: 'none', padding: 0 }}>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/bubblegum">Bubblegum</Link></li>
-                    <li><Link to="/shoelaces">Shoelaces</Link></li>
+        <Route render={({ location }) => (
+            <div style={styles.fill}>
+                <Route exact path="/" render={() => (
+                    <Redirect to="/10/90/50"/>
+                )}/>
+
+                <ul style={styles.nav}>
+                    <NavLink to="/10/90/50">Red</NavLink>
+                    <NavLink to="/120/100/40">Green</NavLink>
+                    <NavLink to="/200/100/40">Blue</NavLink>
+                    <NavLink to="/310/100/50">Pink</NavLink>
                 </ul>
 
-                {routes.map((route, index) => (
-                    // You can render a <Route> in as many places
-                    // as you want in your app. It will render along
-                    // with any other <Route>s that also match the URL.
-                    // So, a sidebar or breadcrumbs or anything else
-                    // that requires you to render multiple things
-                    // in multiple places at the same URL is nothing
-                    // more than multiple <Route>s.
-                    <Route
-                        key={index}
-                        path={route.path}
-                        exact={route.exact}
-                        component={route.sidebar}
-                    />
-                ))}
+                <div style={styles.content}>
+                    <ReactCSSTransitionGroup
+                        transitionName="fade"
+                        transitionEnterTimeout={300}
+                        transitionLeaveTimeout={300}
+                    >
+                        {/* no different than other usage of
+                ReactCSSTransitionGroup, just make
+                sure to pass `location` to `Route`
+                so it can match the old location
+                as it animates out
+            */}
+                        <Route
+                            location={location}
+                            key={location.key}
+                            path="/:h/:s/:l"
+                            component={HSL}
+                        />
+                    </ReactCSSTransitionGroup>
+                </div>
             </div>
-
-            <div style={{ flex: 1, padding: '10px' }}>
-                {routes.map((route, index) => (
-                    // Render more <Route>s with the same paths as
-                    // above, but different components this time.
-                    <Route
-                        key={index}
-                        path={route.path}
-                        exact={route.exact}
-                        component={route.main}
-                    />
-                ))}
-            </div>
-        </div>
+        )}/>
     </Router>
 )
 
-export default SidebarExample
+const NavLink = (props) => (
+    <li style={styles.navItem}>
+        <Link {...props} style={{ color: 'inherit' }}/>
+    </li>
+)
+
+const HSL = ({ match: { params } }) => (
+    <div style={{
+        ...styles.fill,
+        ...styles.hsl,
+        background: `hsl(${params.h}, ${params.s}%, ${params.l}%)`
+    }}>hsl({params.h}, {params.s}%, {params.l}%)</div>
+)
+
+const styles = {}
+
+styles.fill = {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0
+}
+
+styles.content = {
+    ...styles.fill,
+    top: '40px',
+    textAlign: 'center'
+}
+
+styles.nav = {
+    padding: 0,
+    margin: 0,
+    position: 'absolute',
+    top: 0,
+    height: '40px',
+    width: '100%',
+    display: 'flex'
+}
+
+styles.navItem = {
+    textAlign: 'center',
+    flex: 1,
+    listStyleType: 'none',
+    padding: '10px'
+}
+
+styles.hsl  = {
+    ...styles.fill,
+    color: 'white',
+    paddingTop: '20px',
+    fontSize: '30px'
+}
+
+export default AnimationExample
