b51cc309907ed1dffc9de3e8844893509893aa84
worldsong <2060884089@qq.com>
2018-04-04 17:00:36 +0800

    router-deep-learning:侧边导航栏。

--- Senior/lesson6/react-router-deep-learning/demo-app/src/App.js
+++ Senior/lesson6/react-router-deep-learning/demo-app/src/App.js
@@ -5,39 +5,71 @@ import {
     Link
 } from 'react-router-dom'
 
-const PEEPS = [
-    { id: 0, name: 'Michelle', friends: [ 1, 2, 3 ] },
-    { id: 1, name: 'Sean', friends: [ 0, 3 ] },
-    { id: 2, name: 'Kim', friends: [ 0, 1, 3 ], },
-    { id: 3, name: 'David', friends: [ 1, 2 ] }
+// Each logical "route" has two components, one for
+// the sidebar and one for the main area. We want to
+// render both of them in different places when the
+// path matches the current URL.
+const routes = [
+    { path: '/',
+        exact: true,
+        sidebar: () => <div>home!</div>,
+        main: () => <h2>Home</h2>
+    },
+    { path: '/bubblegum',
+        sidebar: () => <div>bubblegum!</div>,
+        main: () => <h2>Bubblegum</h2>
+    },
+    { path: '/shoelaces',
+        sidebar: () => <div>shoelaces!</div>,
+        main: () => <h2>Shoelaces</h2>
+    }
 ]
 
-const find = (id) => PEEPS.find(p => p.id == id)
-
-const RecursiveExample = () => (
+const SidebarExample = () => (
     <Router>
-        <Person match={{ params: { id: 0 }, url: '' }}/>
+        <div style={{ display: 'flex' }}>
+            <div style={{
+                padding: '10px',
+                width: '40%',
+                background: '#f0f0f0'
+            }}>
+                <ul style={{ listStyleType: 'none', padding: 0 }}>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/bubblegum">Bubblegum</Link></li>
+                    <li><Link to="/shoelaces">Shoelaces</Link></li>
+                </ul>
+
+                {routes.map((route, index) => (
+                    // You can render a <Route> in as many places
+                    // as you want in your app. It will render along
+                    // with any other <Route>s that also match the URL.
+                    // So, a sidebar or breadcrumbs or anything else
+                    // that requires you to render multiple things
+                    // in multiple places at the same URL is nothing
+                    // more than multiple <Route>s.
+                    <Route
+                        key={index}
+                        path={route.path}
+                        exact={route.exact}
+                        component={route.sidebar}
+                    />
+                ))}
+            </div>
+
+            <div style={{ flex: 1, padding: '10px' }}>
+                {routes.map((route, index) => (
+                    // Render more <Route>s with the same paths as
+                    // above, but different components this time.
+                    <Route
+                        key={index}
+                        path={route.path}
+                        exact={route.exact}
+                        component={route.main}
+                    />
+                ))}
+            </div>
+        </div>
     </Router>
 )
 
-const Person = ({ match }) => {
-    const person = find(match.params.id)
-
-    return (
-        <div>
-            <h3>{person.name}’s Friends</h3>
-            <ul>
-                {person.friends.map(id => (
-                    <li key={id}>
-                        <Link to={`${match.url}/${id}`}>
-                            {find(id).name}
-                        </Link>
-                    </li>
-                ))}
-            </ul>
-            <Route path={`${match.url}/:id`} component={Person}/>
-        </div>
-    )
-}
-
-export default RecursiveExample
+export default SidebarExample
