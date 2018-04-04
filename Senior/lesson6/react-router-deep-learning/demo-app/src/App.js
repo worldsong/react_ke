d5a85e9c884cdcc03b87a376bf42547019678fad
worldsong <2060884089@qq.com>
2018-04-04 17:19:18 +0800

    router-deep-learning:模态框。

--- Senior/lesson6/react-router-deep-learning/demo-app/src/App.js
+++ Senior/lesson6/react-router-deep-learning/demo-app/src/App.js
@@ -1,78 +1,176 @@
 import React from 'react'
 import {
     BrowserRouter as Router,
+    Switch,
     Route,
     Link
 } from 'react-router-dom'
 
-// Some folks find value in a centralized route config.
-// A route config is just data. React is great at mapping
-// data into components, and <Route> is a component.
+// This example shows how to render two different screens
+// (or the same screen in a different context) at the same url,
+// depending on you got there.
+//
+// Click the colors and see them full screen, then "visit the
+// gallery" and click on the colors. Note the URL and the component
+// are the same as before but now we see them inside a modal
+// on top of the old screen.
 
-////////////////////////////////////////////////////////////
-// first our route components
-const Main = () => <h2>Main</h2>
+class ModalSwitch extends React.Component {
 
-const Sandwiches = () => <h2>Sandwiches</h2>
+    // We can pass a location to <Switch/> that will tell it to
+    // ignore the router's current location and use the location
+    // prop instead.
+    //
+    // We can also use "location state" to tell the app the user
+    // wants to go to `/images/2` in a modal, rather than as the
+    // main page, keeping the gallery visible behind it.
+    //
+    // Normally, `/images/2` wouldn't match the gallery at `/`.
+    // So, to get both screens to render, we can save the old
+    // location and pass it to Switch, so it will think the location
+    // is still `/` even though its `/images/2`.
+    previousLocation = this.props.location
 
-const Tacos = ({ routes }) => (
+    componentWillUpdate(nextProps) {
+        const { location } = this.props
+        // set previousLocation if props.location is not modal
+        if (
+            nextProps.history.action !== 'POP' &&
+            (!location.state || !location.state.modal)
+        ) {
+            this.previousLocation = this.props.location
+        }
+    }
+
+    render() {
+        const { location } = this.props
+        const isModal = !!(
+            location.state &&
+            location.state.modal &&
+            this.previousLocation !== location // not initial render
+        )
+        return (
+            <div>
+                <Switch location={isModal ? this.previousLocation : location}>
+                    <Route exact path='/' component={Home}/>
+                    <Route path='/gallery' component={Gallery}/>
+                    <Route path='/img/:id' component={ImageView}/>
+                </Switch>
+                {isModal ? <Route path='/img/:id' component={Modal} /> : null}
+            </div>
+        )
+    }
+}
+
+const IMAGES = [
+    { id: 0, title: 'Dark Orchid', color: 'DarkOrchid' },
+    { id: 1, title: 'Lime Green', color: 'LimeGreen' },
+    { id: 2, title: 'Tomato', color: 'Tomato' },
+    { id: 3, title: 'Seven Ate Nine', color: '#789' },
+    { id: 4, title: 'Crimson', color: 'Crimson' }
+]
+
+const Thumbnail = ({ color }) =>
+    <div style={{
+        width: 50,
+        height: 50,
+        background: color
+    }}/>
+
+const Image = ({ color }) =>
+    <div style={{
+        width: '100%',
+        height: 400,
+        background: color
+    }}></div>
+
+const Home = () => (
     <div>
-        <h2>Tacos</h2>
+        <Link to='/gallery'>Visit the Gallery</Link>
+        <h2>Featured Images</h2>
         <ul>
-            <li><Link to="/tacos/bus">Bus</Link></li>
-            <li><Link to="/tacos/cart">Cart</Link></li>
+            <li><Link to='/img/2'>Tomato</Link></li>
+            <li><Link to='/img/4'>Crimson</Link></li>
         </ul>
+    </div>
+)
 
-        {routes.map((route, i) => (
-            <RouteWithSubRoutes key={i} {...route}/>
+const Gallery = () => (
+    <div>
+        {IMAGES.map(i => (
+            <Link
+                key={i.id}
+                to={{
+                    pathname: `/img/${i.id}`,
+                    // this is the trick!
+                    state: { modal: true }
+                }}
+            >
+                <Thumbnail color={i.color} />
+                <p>{i.title}</p>
+            </Link>
         ))}
     </div>
 )
 
-const Bus = () => <h3>Bus</h3>
-const Cart = () => <h3>Cart</h3>
-
-////////////////////////////////////////////////////////////
-// then our route config
-const routes = [
-    { path: '/sandwiches',
-        component: Sandwiches
-    },
-    { path: '/tacos',
-        component: Tacos,
-        routes: [
-            { path: '/tacos/bus',
-                component: Bus
-            },
-            { path: '/tacos/cart',
-                component: Cart
-            }
-        ]
+const ImageView = ({ match }) => {
+    const image = IMAGES[parseInt(match.params.id, 10)]
+    if (!image) {
+        return <div>Image not found</div>
     }
-]
 
-// wrap <Route> and use this everywhere instead, then when
-// sub routes are added to any route it'll work
-const RouteWithSubRoutes = (route) => (
-    <Route path={route.path} render={props => (
-        // pass the sub-routes down to keep nesting
-        <route.component {...props} routes={route.routes}/>
-    )}/>
-)
-
-const RouteConfigExample = () => (
-    <Router>
+    return (
         <div>
-            <ul>
-                <li><Link to="/tacos">Tacos</Link></li>
-                <li><Link to="/sandwiches">Sandwiches</Link></li>
-            </ul>
-
-            {routes.map((route, i) => (
-                <RouteWithSubRoutes key={i} {...route}/>
-            ))}
+            <h1>{image.title}</h1>
+            <Image color={image.color} />
         </div>
+    )
+}
+
+const Modal = ({ match, history }) => {
+    const image = IMAGES[parseInt(match.params.id, 10)]
+    if (!image) {
+        return null
+    }
+    const back = (e) => {
+        e.stopPropagation()
+        history.goBack()
+    }
+    return (
+        <div
+            onClick={back}
+            style={{
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                bottom: 0,
+                right: 0,
+                background: 'rgba(0, 0, 0, 0.15)'
+            }}
+        >
+            <div className='modal' style={{
+                position: 'absolute',
+                background: '#fff',
+                top: 25,
+                left: '10%',
+                right: '10%',
+                padding: 15,
+                border: '2px solid #444'
+            }}>
+                <h1>{image.title}</h1>
+                <Image color={image.color} />
+                <button type='button' onClick={back}>
+                    Close
+                </button>
+            </div>
+        </div>
+    )
+}
+
+const ModalGallery = () => (
+    <Router>
+        <Route component={ModalSwitch} />
     </Router>
 )
 
-export default RouteConfigExample
+export default ModalGallery
