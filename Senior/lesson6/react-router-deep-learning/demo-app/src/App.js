e71d9f10572de80f6ae059cd3aa592b2186a2846
worldsong <2060884089@qq.com>
2018-04-04 17:10:09 +0800

    router-deep-learning:双关匹配，ambiguous matches。

--- Senior/lesson6/react-router-deep-learning/demo-app/src/App.js
+++ Senior/lesson6/react-router-deep-learning/demo-app/src/App.js
@@ -1,116 +1,49 @@
 import React from 'react'
-import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
 import {
     BrowserRouter as Router,
     Route,
     Link,
-    Redirect
+    Switch
 } from 'react-router-dom'
 
-/* you'll need this CSS somewhere
-.fade-enter {
-  opacity: 0;
-  z-index: 1;
-}
-
-.fade-enter.fade-enter-active {
-  opacity: 1;
-  transition: opacity 250ms ease-in;
-}
-*/
-
-const AnimationExample = () => (
+const AmbiguousExample = () => (
     <Router>
-        <Route render={({ location }) => (
-            <div style={styles.fill}>
-                <Route exact path="/" render={() => (
-                    <Redirect to="/10/90/50"/>
-                )}/>
+        <div>
+            <ul>
+                <li><Link to="/about">About Us (static)</Link></li>
+                <li><Link to="/company">Company (static)</Link></li>
+                <li><Link to="/kim">Kim (dynamic)</Link></li>
+                <li><Link to="/chris">Chris (dynamic)</Link></li>
+            </ul>
 
-                <ul style={styles.nav}>
-                    <NavLink to="/10/90/50">Red</NavLink>
-                    <NavLink to="/120/100/40">Green</NavLink>
-                    <NavLink to="/200/100/40">Blue</NavLink>
-                    <NavLink to="/310/100/50">Pink</NavLink>
-                </ul>
-
-                <div style={styles.content}>
-                    <ReactCSSTransitionGroup
-                        transitionName="fade"
-                        transitionEnterTimeout={300}
-                        transitionLeaveTimeout={300}
-                    >
-                        {/* no different than other usage of
-                ReactCSSTransitionGroup, just make
-                sure to pass `location` to `Route`
-                so it can match the old location
-                as it animates out
-            */}
-                        <Route
-                            location={location}
-                            key={location.key}
-                            path="/:h/:s/:l"
-                            component={HSL}
-                        />
-                    </ReactCSSTransitionGroup>
-                </div>
-            </div>
-        )}/>
+            {/*
+          Sometimes you want to have a whitelist of static paths
+          like "/about" and "/company" but also allow for dynamic
+          patterns like "/:user". The problem is that "/about"
+          is ambiguous and will match both "/about" and "/:user".
+          Most routers have an algorithm to decide for you what
+          it will match since they only allow you to match one
+          "route". React Router lets you match in multiple places
+          on purpose (sidebars, breadcrumbs, etc). So, when you
+          want to clear up any ambiguous matching, and not match
+          "/about" to "/:user", just wrap your <Route>s in a
+          <Switch>. It will render the first one that matches.
+      */}
+            <Switch>
+                <Route path="/about" component={About}/>
+                <Route path="/company" component={Company}/>
+                <Route path="/:user" component={User}/>
+            </Switch>
+        </div>
     </Router>
 )
 
-const NavLink = (props) => (
-    <li style={styles.navItem}>
-        <Link {...props} style={{ color: 'inherit' }}/>
-    </li>
+const About = () => <h2>About</h2>
+const Company = () => <h2>Company</h2>
+const User = ({ match }) => (
+    <div>
+        <h2>User: {match.params.user}</h2>
+    </div>
 )
 
-const HSL = ({ match: { params } }) => (
-    <div style={{
-        ...styles.fill,
-        ...styles.hsl,
-        background: `hsl(${params.h}, ${params.s}%, ${params.l}%)`
-    }}>hsl({params.h}, {params.s}%, {params.l}%)</div>
-)
-
-const styles = {}
-
-styles.fill = {
-    position: 'absolute',
-    left: 0,
-    right: 0,
-    top: 0,
-    bottom: 0
-}
-
-styles.content = {
-    ...styles.fill,
-    top: '40px',
-    textAlign: 'center'
-}
-
-styles.nav = {
-    padding: 0,
-    margin: 0,
-    position: 'absolute',
-    top: 0,
-    height: '40px',
-    width: '100%',
-    display: 'flex'
-}
-
-styles.navItem = {
-    textAlign: 'center',
-    flex: 1,
-    listStyleType: 'none',
-    padding: '10px'
-}
-
-styles.hsl  = {
-    ...styles.fill,
-    color: 'white',
-    paddingTop: '20px',
-    fontSize: '30px'
-}
-
-export default AnimationExample
+export default AmbiguousExample
