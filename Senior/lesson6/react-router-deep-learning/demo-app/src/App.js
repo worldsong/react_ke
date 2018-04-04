71ef6e232fcd5cf1e32581c18c6e31f8b23f0730
worldsong <2060884089@qq.com>
2018-04-04 15:58:51 +0800

    router-deep-learning: 阻止过渡，preventing transitions。

--- Senior/lesson6/react-router-deep-learning/demo-app/src/App.js
+++ Senior/lesson6/react-router-deep-learning/demo-app/src/App.js
@@ -2,39 +2,72 @@ import React from 'react'
 import {
     BrowserRouter as Router,
     Route,
-    Link
+    Link,
+    Prompt
 } from 'react-router-dom'
 
-const CustomLinkExample = () => (
+const PreventingTransitionsExample = () => (
     <Router>
         <div>
-            <OldSchoolMenuLink activeOnlyWhenExact={true} to="/" label="Home"/>
-            <OldSchoolMenuLink to="/about" label="About"/>
-            <hr/>
-            <Route exact path="/" component={Home}/>
-            <Route path="/about" component={About}/>
+            <ul>
+                <li><Link to="/">Form</Link></li>
+                <li><Link to="/one">One</Link></li>
+                <li><Link to="/two">Two</Link></li>
+            </ul>
+            <Route path="/" exact component={Form}/>
+            <Route path="/one" render={() => <h3>One</h3>}/>
+            <Route path="/two" render={() => <h3>Two</h3>}/>
         </div>
     </Router>
 )
 
-const OldSchoolMenuLink = ({ label, to, activeOnlyWhenExact }) => (
-    <Route path={to} exact={activeOnlyWhenExact} children={({ match }) => (
-        <div className={match ? 'active' : ''}>
-            {match ? '> ' : ''}<Link to={to}>{label}</Link>
-        </div>
-    )}/>
-)
+class Form extends React.Component {
+    state = {
+        isBlocking: false
+    }
 
-const Home = () => (
-    <div>
-        <h2>Home</h2>
-    </div>
-)
+    render() {
+        const { isBlocking } = this.state
 
-const About = () => (
-    <div>
-        <h2>About</h2>
-    </div>
-)
+        return (
+            <form
+                onSubmit={event => {
+                    event.preventDefault()
+                    event.target.reset()
+                    this.setState({
+                        isBlocking: false
+                    })
+                }}
+            >
+                <Prompt
+                    when={isBlocking}
+                    message={location => (
+                        `Are you sure you want to go to ${location.pathname}`
+                    )}
+                />
 
-export default CustomLinkExample
+                <p>
+                    Blocking? {isBlocking ? 'Yes, click a link or the back button' : 'Nope'}
+                </p>
+
+                <p>
+                    <input
+                        size="50"
+                        placeholder="type something to block transitions"
+                        onChange={event => {
+                            this.setState({
+                                isBlocking: event.target.value.length > 0
+                            })
+                        }}
+                    />
+                </p>
+
+                <p>
+                    <button>Submit to stop blocking</button>
+                </p>
+            </form>
+        )
+    }
+}
+
+export default PreventingTransitionsExample
